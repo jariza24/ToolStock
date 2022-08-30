5c62eeb682fd2ed6572e368f79b666ba41bf3794
Jesús David Ariza Castro <jesus.d1999jym@gmail.com>
2022-08-30 09:03:57 -0500

    se agrega la carpeta static y se especifica la ruta en el archivo settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -118,12 +118,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [ 
+   os.path.join(BASE_DIR, 'core/static/') 
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
