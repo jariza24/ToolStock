ab538dedc1c53611cdbc8c3d839a6071fc43d141
Jesús David Ariza Castro <jesus.d1999jym@gmail.com>
2022-09-09 16:18:50 -0500

    se crean los modelos de la base de datos

---
+++ core/toolstockapp/choices.py
@@ -0,0 +1,30 @@
+grupos = (
+    ('1','Cerrajeria'),
+    ('2','Enchapes'),
+    ('3','Pisos'),
+    ('4','Tuberia PVC Electrica'),
+    ('5','Tuberia PVC Sanitaria'),
+    ('6','Pinturas')
+)
+
+proveedores = (
+    ('1','Alfagres S.A.'),
+    ('2','Cerámica Italia S.A.'),
+    ('3','Mexichem Colombia S.A.S.'),
+    ('4','Pintuco Colombia S.A.'),
+    ('5','Almacenes Corona S.A.'),
+    ('6','Durman Colombia S.A.S.'),
+    ('7','Constructor Sodimac Corona Almacén'),
+    ('8','Coval Comercial S.A.'),
+    ('9','Allegion Colombia S.A.S.')
+)
+
+unidades = (
+    ('1','Unidad'),
+    ('2','Libra'),
+    ('3','m2'),
+    ('4','m'),
+    ('5','Kg'),
+    ('6','Gl'),
+    ('7','Gal')
+)
--- core/toolstockapp/models.py
+++ core/toolstockapp/models.py
@@ -1,3 +1,27 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+class Grupo(models.Model):
+    nombre = models.CharField(max_length=30)
+
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=30)
+
+class Unidad(models.Model):
+    medida = models.CharField(max_length=10)
+
+class Proveedor(models.Model):
+    nombre = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=50)
+    ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE)
+    #falta id de la ciudad llave foranea
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.FloatField()
+    unidad = models.ForeignKey(Unidad, on_delete=models.CASCADE)
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE)
+    #falta id Unidad, id Proveedor, id Grupo
