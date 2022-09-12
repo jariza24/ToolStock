6704a6273aedcb4d5b0772c2f3769944166e8fc3
Jesús David Ariza Castro <jesus.d1999jym@gmail.com>
2022-09-12 15:43:50 -0500

    se modifica el archivo gitignore

--- core/toolstockapp/views.py
+++ core/toolstockapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect
+from .models import *
 
 # Create your views here.
 
