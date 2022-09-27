bd864634d1c82f6647d3e72f943e4eef3d8cc772
Jesús David Ariza Castro <jesus.d1999jym@gmail.com>
2022-09-27 10:40:38 -0500

    se modifica el archivo settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -125,10 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'core/static/'),
 )
 
 # Default primary key field type
