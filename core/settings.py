c9e3ffdb52e104c67ca413788758377c010847c0
Jesús David Ariza Castro <jesus.d1999jym@gmail.com>
2022-09-30 09:45:13 -0500

    se modifica el archivo settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-63*4zm_fuj2unbzsq7#@0sf-uzd*v$z*uufr-y)@0*)0!h)7c_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'core/static'),
 )
 
 # Default primary key field type
